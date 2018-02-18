9b30a1b194d2be3889bc7a34a657a3a62b18e886
Changsi An <anchangsi@gmail.com>
2018-02-17 20:04:55 -0800

    Builds production version with Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,41 @@
 let gulp = require('gulp');
 let ts = require('gulp-typescript');
+let webpackO = require('webpack');
+let webpack = require('webpack-stream');
 let less = require('gulp-less');
 let sourcemaps = require('gulp-sourcemaps');
+let del = require('del');
 
 let tsProject = ts.createProject('tsconfig.json');
+
+const webpackConfig = {
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+
+    node: {
+        __dirname: true
+    },
+
+    module: {
+        loaders: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+        ],
+    },
+
+    output: {
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new webpackO.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ],
+
+    externals: {
+    }
+};
 
 gulp.task('copy', function() {
     return gulp.src(['./package.json', './src/style.less'])
@@ -31,6 +63,33 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./out/'));
 });
 
+gulp.task('ts-webpack-main', () => {
+    let _webpackConfig = Object.assign({}, webpackConfig);
+    _webpackConfig.output.filename = 'main.js';
+    _webpackConfig.target = 'electron-main';
 
-gulp.task('build', ['ts', 'less']);
-gulp.task('default', ['build', 'copy']);
+    return gulp.src('./src/main.ts')
+        .pipe(webpack(_webpackConfig))
+        .pipe(gulp.dest('./out/'));
+});
+
+gulp.task('ts-webpack-renderer', ['ts-webpack-main'], () => {
+
+    let _webpackConfig = Object.assign({}, webpackConfig);
+    _webpackConfig.output.filename = 'renderer.js';
+    _webpackConfig.target = 'electron-renderer';
+
+    return gulp.src('./src/renderer.tsx')
+        .pipe(webpack(_webpackConfig))
+        .pipe(gulp.dest('./out/'));
+});
+
+gulp.task('ts-webpack', ['ts-webpack-renderer']);
+
+gulp.task('clean', () => {
+    return del.sync('./out/**/*.*');
+});
+
+gulp.task('build', ['clean', 'ts', 'less', 'copy']);
+gulp.task('build-prod', ['clean', 'ts-webpack', 'less', 'copy']);
+gulp.task('default', ['build']);
